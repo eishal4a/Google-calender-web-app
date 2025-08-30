91fb3e097a094f218cbeb4517f11f9d86c820b9b
Eishal <eishal.cecos@gmail.com>
2025-08-30 23:27:22 +0500

    p
    p

--- Calnder-fullstack/src/components/calender.jsx
+++ Calnder-fullstack/src/components/calender.jsx
@@ -3,7 +3,7 @@ import './calender.css'
 const calender = () => {
   return (
     <div>
-      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=UTC&showPrint=0&src=ZWlzaGFsLmNlY29zQGdtYWlsLmNvbQ&src=OGYyODEyM2Q3ODFhODUzMTI5ZmZmNTk4ZjZhZmY3ZDU3ZjY1YTRmMDkzNTU5ZDVmZTRiOTVmNGI1NGUzYzEzZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=NTlmN2RjMzg1YzcwYTc3MDMxZjZiN2M5ZGM1ZGVkZjZlNzVlOWI0ZmExZGVmNzQxMzg3MTk2ZWI1YjFlYWZjMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4ucGsjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&src=Y2xhc3Nyb29tMTAxMjU2Njg0MzQ5NTEyNzI3NDY5QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039be5&color=%237cb342&color=%23d81b60&color=%230b8043&color=%23137333" frameborder="0">
+      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=UTC&showPrint=0&src=ZWlzaGFsLmNlY29zQGdtYWlsLmNvbQ&src=OGYyODEyM2Q3ODFhODUzMTI5ZmZmNTk4ZjZhZmY3ZDU3ZjY1YTRmMDkzNTU5ZDVmZTRiOTVmNGI1NGUzYzEzZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=NTlmN2RjMzg1YzcwYTc3MDMxZjZiN2M5ZGM1ZGVkZjZlNzVlOWI0ZmExZGVmNzQxMzg3MTk2ZWI1YjFlYWZjMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4ucGsjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&src=Y2xhc3Nyb29tMTAxMjU2Njg0MzQ5NTEyNzI3NDY5QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039be5&color=%237cb342&color=%23d81b60&color=%230b8043&color=%23137333" frameborder="0" style={{width:"700px",height:"700px"}}>
 
       </iframe>
     </div>
